ce4629b99f7a57362042e47b67e3f15726e979cf
Ken Suon <ken.su_dev@hotmail.com>
2021-10-06 23:02:57 +0200

    Initial commit

---
+++ app/src/main/java/com/suonk/testnavigationcomponent/ui/fragments/MainFragment.kt
@@ -0,0 +1,46 @@
+package com.suonk.testnavigationcomponent.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.suonk.testnavigationcomponent.R
+import com.suonk.testnavigationcomponent.databinding.FragmentMainBinding
+
+class MainFragment : Fragment(), View.OnClickListener {
+
+    private var binding: FragmentMainBinding? = null
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        binding?.sendMoneyBtn?.setOnClickListener(this)
+        binding?.viewBalanceBtn?.setOnClickListener(this)
+        binding?.viewTransactionsBtn?.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.view_transactions_btn -> navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.view_balance_btn -> navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+            R.id.send_money_btn -> navController.navigate(R.id.action_mainFragment_to_chooserRecipientFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
